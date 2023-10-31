dcac6c4a7227f1350077964d412b11e564512b33
PXTN45 <120036577+PXTN45@users.noreply.github.com>
2023-10-31 16:43:16 +0700

    .

--- app/page.js
+++ app/page.js
@@ -1,95 +1,132 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import React, { useState, useEffect } from "react";
+import { initializeConnector } from "@web3-react/core";
+import { MetaMask } from "@web3-react/metamask";
+import { ethers } from "ethers";
+import { parseUnits } from "@ethersproject/units";
+import abi from "./abi.json";
+import Navbar from "@/component/navbar";
+import { _ethers } from "ethers";
+import { formatEther } from "@ethersproject/units";
+// import * as React from 'react';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
-export default function Home() {
+const [metaMask, hooks] = initializeConnector(
+  (actions) => new MetaMask({ actions })
+);
+const { useChainId, useAccounts, useIsActivating, useIsActive, useProvider } =
+  hooks;
+const contractChain = 11155111;
+const contractAddress = "0x3e106289ab49aba62775f8570908a3ab34792b05";
+
+export default function Page() {
+  const chainId = useChainId();
+  const accounts = useAccounts();
+  const isActive = useIsActive();
+
+  const provider = useProvider();
+  const [error, setError] = useState(undefined);
+
+  useEffect(() => {
+    void metaMask.connectEagerly().catch(() => {
+      console.debug("Failed to connect eagerly to metamask");
+    });
+  }, []);
+
+  const handleConnect = () => {
+    metaMask.activate(contractChain);
+  };
+
+  const handleDisconnect = () => {
+    metaMask.resetState();
+  };
+  const [balance, setBalance] = useState("");
+  useEffect(() => {
+    const fetchBalance = async () => {
+      const signer = provider.getSigner();
+      const smartContract = new ethers.Contract(contractAddress, abi, signer);
+      const myBalance = await smartContract.balanceOf(accounts[0]);
+      console.log(formatEther(myBalance));
+      setBalance(formatEther(myBalance));
+    };
+    if (isActive) {
+      fetchBalance();
+    }
+  }, [isActive]);
+
+  const [pxtnValue, setPxtnValue] = useState(0);
+  const handleSetPxtnValue = (event) => {
+    setPxtnValue(event.target.value);
+  };
+  const handleBuy = async () => {
+    try {
+      if (pxtnValue <= 0) {
+        return;
+      }
+      const signer = provider.getSigner();
+      const smartContract = new ethers.Contract(contractAddress, abi, signer);
+      const buyValue = parseUnits(pxtnValue.toString(), "ether");
+      const tx = await smartContract.buy({
+        value: buyValue.toString(),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <Navbar
+        accounts={accounts}
+        chainId={chainId}
+        isActive={isActive}
+        provider={provider}
+        handleConnect={handleConnect}
+        handleDisconnect={handleDisconnect}
+      />
+      {/* {isActive && <div> balance = {balance} </div>} */}
+      {isActive && (
+        <div className="container">
+          <div className="card">
+            <Box
+              component="form"
+              sx={{
+                "& > :not(style)": { m: 1, width: "50ch" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <h1>My wallet balance</h1>
+              <TextField label="Address" value={accounts ? accounts[0] : ""} />
+              <TextField label="PXTN Token" value={balance} />
+              <h1>Buy PXTN Token</h1>
+              <TextField
+                required
+                id="outlined-required"
+                label="Enter amount of Ether you want to buy PXTN Token"
+                defaultValue=""
+                type="number"
+                onChange={handleSetPxtnValue}
+              />
+              <Stack
+                spacing={2}
+                direction="row"
+                sx={{ justifyContent: "center" }}
+              >
+                <Button sx={{ width: "500px" }} onClick={handleBuy} variant="contained">
+                  Buy PXTN Token
+                </Button>
+              </Stack>
+              {/* <p>chainId: {chainId}</p>
+              <p>isActive: {isActive.toString()}</p>
+              <p>accounts: {accounts ? accounts[0] : ""}</p> */}
+            </Box>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      )}
+    </div>
+  );
 }
---
+++ component/navbar.js
@@ -0,0 +1,78 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { initializeConnector } from "@web3-react/core";
+import { MetaMask } from "@web3-react/metamask";
+import { ethers } from "ethers";
+import { formatEther, parseUnits } from "@ethersproject/units";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Page from "@/app/page";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import { Balance } from "@mui/icons-material";
+const [metaMask, hooks] = initializeConnector(
+  (actions) => new MetaMask({ actions })
+);
+
+const contractChain = 11155111;
+const contractAddress = "0x3e106289ab49aba62775f8570908a3ab34792b05";
+const Navbar = ({
+  handleConnect,
+  handleDisconnect,
+  isActive,
+  accounts,
+  provider,
+}) => {
+  useEffect(() => {
+    void metaMask.connectEagerly().catch(() => {
+      console.debug("Failed to connect eagerly to metamask");
+    });
+  }, []);
+  const [error, setError] = useState(undefined);
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            BlockChain
+          </Typography>
+
+          {isActive ? (
+            <Stack direction="row" spacing={1}>
+              <Chip label={accounts} variant="outlined" />
+              <Button color="inherit" onClick={handleDisconnect}>
+                {" "}
+                Disconnect{" "}
+              </Button>
+            </Stack>
+          ) : (
+            <Button color="inherit" onClick={handleConnect}>
+              {" "}
+              Connect{" "}
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+
+
+
+
+    </Box>
+  );
+};
+export default Navbar;
